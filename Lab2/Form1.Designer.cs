ec322a655fab27c275d4226a125270789644f14f
rexx.magtar <rexx.magtar@gmail.com>
2021-02-21 13:58:53 +0300

    lab done

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitGetInfoButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Resolution = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColorDepth = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CompressionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.folderSelectButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // submitGetInfoButton
+            // 
+            this.submitGetInfoButton.Location = new System.Drawing.Point(183, 60);
+            this.submitGetInfoButton.Name = "submitGetInfoButton";
+            this.submitGetInfoButton.Size = new System.Drawing.Size(110, 23);
+            this.submitGetInfoButton.TabIndex = 2;
+            this.submitGetInfoButton.Text = "Select image";
+            this.submitGetInfoButton.UseVisualStyleBackColor = true;
+            this.submitGetInfoButton.Click += new System.EventHandler(this.submitGetInfoButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.Size,
+            this.Resolution,
+            this.ColorDepth,
+            this.CompressionColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1080, 338);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.HeaderText = "Name";
+            this.NameColumn.MinimumWidth = 6;
+            this.NameColumn.Name = "NameColumn";
+            this.NameColumn.ReadOnly = true;
+            this.NameColumn.Width = 125;
+            // 
+            // Size
+            // 
+            this.Size.HeaderText = "Size";
+            this.Size.MinimumWidth = 6;
+            this.Size.Name = "Size";
+            this.Size.ReadOnly = true;
+            this.Size.Width = 125;
+            // 
+            // Resolution
+            // 
+            this.Resolution.HeaderText = "Resolution";
+            this.Resolution.MinimumWidth = 6;
+            this.Resolution.Name = "Resolution";
+            this.Resolution.ReadOnly = true;
+            this.Resolution.Width = 125;
+            // 
+            // ColorDepth
+            // 
+            this.ColorDepth.HeaderText = "Color depth";
+            this.ColorDepth.MinimumWidth = 6;
+            this.ColorDepth.Name = "ColorDepth";
+            this.ColorDepth.ReadOnly = true;
+            this.ColorDepth.Width = 125;
+            // 
+            // CompressionColumn
+            // 
+            this.CompressionColumn.HeaderText = "Compression";
+            this.CompressionColumn.MinimumWidth = 6;
+            this.CompressionColumn.Name = "CompressionColumn";
+            this.CompressionColumn.ReadOnly = true;
+            this.CompressionColumn.Width = 125;
+            // 
+            // folderSelectButton
+            // 
+            this.folderSelectButton.Location = new System.Drawing.Point(12, 60);
+            this.folderSelectButton.Name = "folderSelectButton";
+            this.folderSelectButton.Size = new System.Drawing.Size(118, 23);
+            this.folderSelectButton.TabIndex = 6;
+            this.folderSelectButton.Text = "Select folder";
+            this.folderSelectButton.UseVisualStyleBackColor = true;
+            this.folderSelectButton.Click += new System.EventHandler(this.folderSelectButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 574);
+            this.Controls.Add(this.folderSelectButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.submitGetInfoButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button submitGetInfoButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Resolution;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColorDepth;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CompressionColumn;
+        private System.Windows.Forms.Button folderSelectButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+using FreeImageAPI;
+using FreeImageAPI.Metadata;
+using ImageMagick;
+using Directory = System.IO.Directory;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitGetInfoButton_Click(object sender, EventArgs e)
+        {
+            var filePickResult = openFileDialog1.ShowDialog();
+
+            if (filePickResult != DialogResult.OK)
+            {
+                return;
+            }
+
+            try
+            {
+                AddNewImageInfo(openFileDialog1.FileName, true);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void folderSelectButton_Click(object sender, EventArgs e)
+        {
+            var folderResult = folderBrowserDialog1.ShowDialog();
+
+            if (folderResult != DialogResult.OK)
+            {
+                return;
+            }
+
+            dataGridView1.Rows.Clear();
+
+            var files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                try
+                {
+                    AddNewImageInfo(files[i]);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    throw;
+
+                }
+            }
+
+        }
+
+        private void AddNewImageInfo(string imagePath, bool clearTable = false)
+        {
+            string fileName = new FileInfo(imagePath).Name;
+
+
+
+            string imgType = imagePath.Substring(imagePath.LastIndexOf('.')+1, imagePath.Length - 1 - imagePath.LastIndexOf('.'));
+
+            if (imgType.Equals("pcx",StringComparison.OrdinalIgnoreCase))
+            {
+
+
+                FREE_IMAGE_FORMAT formatR = FREE_IMAGE_FORMAT.FIF_UNKNOWN;
+
+                // Try loading the file
+                FIBITMAP dib = FreeImage.LoadEx(imagePath, ref formatR);
+
+
+                uint width = FreeImage.GetWidth(dib);
+
+                uint height = FreeImage.GetHeight(dib);
+
+
+                float horizontalResolution = FreeImage.GetResolutionX(dib);
+
+                float verticalResolution = FreeImage.GetResolutionY(dib);
+
+                //var format = img.PixelFormat;
+
+                uint colorDepth = FreeImage.GetBPP(dib);
+
+                var compression = "";
+
+                int compressionId = 1;
+
+                try
+                {
+                    //compressionId = BitConverter.ToInt16(img.GetPropertyItem(0x0103).Value, 0);
+                }
+                catch (Exception)
+                {
+
+                }
+
+                //switch (compressionId)
+                //{
+                //    case 1:
+                //        compression = "No compression";
+                //        break;
+                //    case 2:
+                //        compression = "CCITT Huffman";
+                //        break;
+                //    case 3:
+                //        compression = "CCITT Group 3";
+                //        break;
+                //    case 4:
+                //        compression = "CCITT Group 4";
+                //        break;
+                //    case 5:
+                //        compression = "LZW";
+                //        break;
+                //    default:
+                //        compression = "Unknown compression";
+                //        break;
+
+                //}
+
+                compression = GetCompressionName(dib);
+
+                if (clearTable)
+                {
+                    dataGridView1.Rows.Clear();
+                }
+
+                int newIndex = dataGridView1.Rows.Add();
+
+                var gridRow = (DataGridViewRow)dataGridView1.Rows[newIndex];
+
+                gridRow.Cells["NameColumn"].Value = fileName;
+                gridRow.Cells["Size"].Value = width + "x" + height;
+                gridRow.Cells["Resolution"].Value = horizontalResolution + "x" + verticalResolution;
+                gridRow.Cells["ColorDepth"].Value = colorDepth;
+                gridRow.Cells["CompressionColumn"].Value = compression;
+
+                FreeImage.UnloadEx(ref dib);
+
+                System.GC.Collect();
+                System.GC.WaitForPendingFinalizers();
+
+            }
+            else
+            {
+
+                Image img = Image.FromFile(imagePath);
+
+
+                int width = img.Width;
+
+                int height = img.Height;
+
+
+                float horizontalResolution = img.HorizontalResolution;
+
+                float verticalResolution = img.VerticalResolution;
+
+                var format = img.PixelFormat;
+
+                int colorDepth = Image.GetPixelFormatSize(format);
+
+                var compression = "";
+
+                int compressionId = 1;
+
+                try
+                {
+                    compressionId = BitConverter.ToInt16(img.GetPropertyItem(0x0103).Value, 0);
+                }
+                catch (Exception)
+                {
+
+                }
+
+                switch (compressionId)
+                {
+                    case 1:
+                        compression = "No compression";
+                        break;
+                    case 2:
+                        compression = "CCITT Huffman";
+                        break;
+                    case 3:
+                        compression = "CCITT Group 3";
+                        break;
+                    case 4:
+                        compression = "CCITT Group 4";
+                        break;
+                    case 5:
+                        compression = "LZW";
+                        break;
+                    default:
+                        compression = "Unknown compression id (" + compressionId + ")";
+                        break;
+
+                }
+
+                if (clearTable)
+                {
+                    dataGridView1.Rows.Clear();
+                }
+
+                int newIndex = dataGridView1.Rows.Add();
+
+                var gridRow = (DataGridViewRow)dataGridView1.Rows[newIndex];
+
+                gridRow.Cells["NameColumn"].Value = fileName;
+                gridRow.Cells["Size"].Value = width + "x" + height;
+                gridRow.Cells["Resolution"].Value = horizontalResolution + "x" + verticalResolution;
+                gridRow.Cells["ColorDepth"].Value = colorDepth;
+                gridRow.Cells["CompressionColumn"].Value = compression;
+
+                System.GC.Collect();
+                System.GC.WaitForPendingFinalizers();
+            }
+
+        }
+
+
+        public string GetCompressionName(FIBITMAP dib)
+        {
+            long _compression;
+
+            if (dib.IsNull)
+                throw new Exception("dib is empty - image haven't been loaded!");
+
+            //Searching tag in metadata.
+            ImageMetadata iMetadata = new ImageMetadata(dib);
+
+            foreach (MetadataModel metadataModel in iMetadata)
+            {
+                if (metadataModel.ToString() == "FIMD_EXIF_MAIN")
+                {
+                    try
+                    {
+                        long.TryParse((metadataModel.GetTag("Compression")?.Value as ushort[])?[0].ToString(), out _compression);
+                    }
+                    catch (Exception ex)
+                    {
+                        return "Unknown";
+                    }
+
+
+                    if (CompressType.ContainsKey(_compression))
+                    {
+                        string _compressionName;
+                        CompressType.TryGetValue(_compression, out _compressionName);
+
+                        if (_compressionName != null)
+                        {
+                            return _compressionName;
+                        }
+                    }
+                }
+            }
+
+            return "Unknown";
+        }
+
+        Dictionary<long, string> CompressType = new Dictionary<long, string>()
+        {
+            {1, "Uncompressed"},
+            {2, "CCITT modified Huffman RLE"},
+            {32773, "PackBits"},
+            {3, "CCITT3"},
+            {4, "CCITT4"},
+            {5, "LZW"},
+            {6, "JPEG_old"},
+            {7, "JPEG_new"},
+            {32946, "DeflatePKZIP"},
+            {8, "DeflateAdobe"},
+            {9, "JBIG_85"},
+            {10, "JBIG_43"},
+            {11, "JPEG"},
+            {12, "JPEG"},
+            {32766, "RLE_NeXT"},
+            {32809, "RLE_ThunderScan"},
+            {32895, "RasterPadding"},
+            {32896, "RLE_LW"},
+            {32897, "RLE_HC"},
+            {32947, "RLE_BL"},
+            {34661, "JBIG"},
+            {34713, "Nikon_NEF"},
+            {34712, "JPEG2000"}
+        };
+    }
+}
